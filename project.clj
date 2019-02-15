4f2d7ff22c2687433b1f044323f81e99c69ca133
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-02-15 14:57:52 +0700

    create project with leiningen

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject learn-clojure "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :aot [learn-clojure.core]
+  :main learn-clojure.core)
